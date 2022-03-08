87db4182514edd484a9a37025b5ca0c84c93d3f3
MatthewMonzon <matthew.monzon23@ycdsbk12.ca>
2022-03-07 23:08:01 -0500

    almost done

--- Sketch.java
+++ Sketch.java
@@ -16,20 +16,47 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    
+    background(135, 206, 235);
+
+    //sun
+    float fltSunX = random(0, 350);
+    float fltSunY = random(0, width);
+    float fltSunD = random(60, 120);
+    ellipse(fltSunX, fltSunY, fltSunD, fltSunD);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    //ground
+	  fill(86, 125, 70);
+    rect(0, 350, 400, 50);
+
+    // red square
+    fill(203, 65, 84);
+    rect(175, 150, 200, 200);
+
+    // door
+    fill(150, 75, 0);
+    rect(225, 225, 50, 125);
+
+    // door knob
+    fill(172, 140, 24);
+    ellipse(260, 300, 10, 10);
+
+    // window
+    fill(135, 206, 235);
+    rect(300, 225, 50, 50);
+    line(325, 225, 325, 275);
+    line(300, 250, 350, 250);
+
+    // roof
+    fill(0, 0, 128);
+    triangle(150, 150, 275, 50, 400, 150);
+
   }
   
   // define other methods down here.
